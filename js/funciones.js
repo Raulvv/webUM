888de39a5d74ee78de792a84a436f35b627adb36
Raulvv <raulvprivate@gmail.com>
2015-02-04 15:13:31 +0100

    Have to put off the second Slide

--- js/funciones.js
+++ js/funciones.js
@@ -1,11 +1,6 @@
-var titleLine;
-
-var lines = {
-	"Linea 1": "img/line1.jpg", "Linea 2": "img/line2.jpg", "Linea 3": "img/line3.jpg", "Linea 4": "img/line4.jpg", "Linea 5": "img/line5.jpg" 
-}
-
 $(document).ready(function(){
-	$('.productPartial').hide();
+	//$('img').attr('src', $('img').attr('src') + '?' + Math.random());
+	//$('.productPartial').hide();
 	$(".menu li").on('click', function(event){
 		if(event.target.text === 'Productos'){
 			$(".submenu").toggleClass("sacaSubMenu");
--- js/urenaController.js
+++ js/urenaController.js
@@ -5,122 +5,122 @@ app.controller('LineaCtrl', [
 function($scope, $filter){
   	$scope.lineas = [
         {
-    		title: 'Linea 1',
-    		description: 'Descripcion 1',
+    		titleLinea: 'Linea 1',
+    		description: 'Descripcion1',
     		image: 'img/linea3-3-g.jpg',
     		productos: [
     		{
-    			title: 'Producto 1',
+    			title: 'Producto1',
     			description: 'The description of the first product. It is not important to see the quality, just the maximum length we can show at the end.',
     			image: 'img/linea1.jpg'
     		},
     		{
-    			title: 'Producto 2',
+    			title: 'Producto2',
     			description: 'The description of the second product. It is not important to see the quality, just the maximum length we can show at the end.',
     			image: 'img/linea1.jpg'
     		},
     		{
-    			title: 'Producto 3',
+    			title: 'Producto3',
     			description: 'The description of the third product. It is not important to see the quality, just the maximum length we can show at the end.',
     			image: 'img/linea1.jpg'
     		}
     		]
     	},
     	{
-    		title: 'Linea 2',
-    		description: 'Descripcion 2',
+    		titleLinea: 'Linea 2',
+    		description: 'Descripcion2',
     		image: 'img/linea3-3-g.jpg',
     		productos: [
     		{
-    			title: 'Producto 1',
+    			title: 'Producto1',
     			description: 'The description of the first product. It is not important to see the quality, just the maximum length we can show at the end.',
     			image: 'img/linea2.jpg'
     		},
     		{
-    			title: 'Producto 2',
+    			title: 'Producto2',
     			description: 'The description of the second product. It is not important to see the quality, just the maximum length we can show at the end.',
     			image: 'img/linea2.jpg'
     		},
     		{
-    			title: 'Producto 3',
+    			title: 'Producto3',
     			description: 'The description of the third product. It is not important to see the quality, just the maximum length we can show at the end.',
     			image: 'img/linea2.jpg'
     		}
     		]
     	},
     	{
-    		title: 'Linea 3',
-    		description: 'Descripcion 3',
+    		titleLinea: 'Linea3',
+    		description: 'Descripcion3',
     		image: 'img/linea3-3-g.jpg',
     		productos: [
     		{
-    			title: 'Producto 1',
+    			title: 'Producto1',
     			description: 'The description of the first product. It is not important to see the quality, just the maximum length we can show at the end.',
     			image: 'img/linea3.jpg'
     		},
     		{
-    			title: 'Producto 2',
+    			title: 'Producto2',
     			description: 'The description of the second product. It is not important to see the quality, just the maximum length we can show at the end.',
     			image: 'img/linea3.jpg'
     		},
     		{
-    			title: 'Producto 3',
+    			title: 'Producto3',
     			description: 'The description of the third product. It is not important to see the quality, just the maximum length we can show at the end.',
     			image: 'img/linea3.jpg'
     		}
     		]
     	},
     	{
-    		title: 'Linea 4',
-    		description: 'Descripcion 4',
+    		titleLinea: 'Linea4',
+    		description: 'Descripcion4',
     		image: 'img/linea3-3-g.jpg',
     		productos: [
     		{
-    			title: 'Producto 1',
+    			title: 'Producto1',
     			description: 'The description of the first product. It is not important to see the quality, just the maximum length we can show at the end.',
     			image: 'img/linea4.jpg'
     		},
     		{
-    			title: 'Producto 2',
+    			title: 'Producto2',
     			description: 'The description of the second product. It is not important to see the quality, just the maximum length we can show at the end.',
     			image: 'img/linea4.jpg'
     		},
     		{
-    			title: 'Producto 3',
+    			title: 'Producto3',
     			description: 'The description of the third product. It is not important to see the quality, just the maximum length we can show at the end.',
     			image: 'img/linea4.jpg'
     		}
     		]
     	},
     	{
-    		title: 'Linea 5',
-    		description: 'Descripcion 5',
+    		titleLinea: 'Linea5',
+    		description: 'Descripcion5',
     		image: 'img/linea3-3-g.jpg',
     		productos: [
     		{
-    			title: 'Producto 1',
+    			title: 'Producto1',
     			description: 'The description of the first product. It is not important to see the quality, just the maximum length we can show at the end.',
     			image: 'img/linea5.jpg'
     		},
     		{
-    			title: 'Producto 2',
+    			title: 'Producto2',
     			description: 'The description of the second product. It is not important to see the quality, just the maximum length we can show at the end.',
     			image: 'img/linea5.jpg'
     		},
     		{
-    			title: 'Producto 3',
+    			title: 'Producto3',
     			description: 'The description of the third product. It is not important to see the quality, just the maximum length we can show at the end.',
     			image: 'img/linea5.jpg'
     		}
     		]
         }
         ];
-        $scope.selected_linea_title = 'Linea 3';
+        $scope.selected_linea_title = 'Linea3';
         $scope.selected_linea;
-        $scope.selectedProduct = 'Producto 1';
+        $scope.selectedProduct = 'Producto1';
 		$scope.getLinea = function(linea){
 			$filter
-			$scope.selected_linea_title = linea.title;
+			$scope.selected_linea_title = linea.titleLinea;
 			$scope.selected_linea = linea;
 			$('.productPartial').show('slow');
 			$('.home').hide('fast');
